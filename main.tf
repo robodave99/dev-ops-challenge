056a3e393bcff2b932af10e6013a342405240d96
Oladapo Dele-David <cube@Oladapos-MacBook-Air.local>
2025-03-30 02:51:03 -0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "mejuri-custom-vpc"
+  }
 }
 
 # Public Subnets
